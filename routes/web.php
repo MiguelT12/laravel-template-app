c2a1c864d7e34478762ac09242ffae3472ed0cd7
David Ruiz <david.ruiz62@educa.madrid.org>
2026-02-11 00:24:20 +0100

    css en la página de login y en la de register

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,15 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
